7afb5a6160e1e23cca76d85235fcb6c8eef072ae
kaylashovlowsky <85142863+kaylashovlowsky@users.noreply.github.com>
2021-09-22 07:38:38 -0700

    Added open-source code link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <WeatherSearch />
+      <br />
+      <h4>
+        <a
+          href="https://github.com/kaylashovlowsky/shecodes-react-mock"
+          alt="Github Link"
+        >
+          Open-source code
+        </a>{" "}
+        by Kayla Shovlowsky
+      </h4>
     </div>
   );
 }
